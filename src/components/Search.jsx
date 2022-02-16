cd084399fae41fd542c326e196a24829d645d2a7
ChineseMalbec <guillenvidalpedro@gmail.com>
2022-02-16 18:44:42 -0500

    Advance

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import styles from "./Search.module.css";
+import { useQuery } from "../hooks/useQuery";
 
 export function Search() {
+  const query = useQuery();
+  const search = query.get("search");
+
   const [searchText, setSearchText] = useState("");
   const history = useHistory();
+
+  useEffect(() => {
+    setSearchText(search || "");
+  }, [search]);
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
     history.push("/?search=" + searchText);
