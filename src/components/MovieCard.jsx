6054ecb87f335b56c126a2ced6fbc7c00608bf4d
ChineseMalbec <guillenvidalpedro@gmail.com>
2022-03-13 18:14:53 -0500

    Prueba

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import styles from "./MovieCard.module.css";
 import { Link } from "react-router-dom";
+// Prueba
 
 export function MovieCard({ movie }) {
   const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
